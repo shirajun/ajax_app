5b9ed6e0d58121ebc7d39a52152482a5921a8098
shirajun <shirajun3891@icloud.com>
2020-10-02 19:53:47 +0900

    既読機能フロントエンド実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ end
 Rails.application.routes.draw do
   root to: 'posts#index'
   #get 'posts/new', to: 'posts#new'
-  post 'posts/:id', to: 'posts#checked'
+  post 'posts', to: 'posts#create'
+  get 'posts/:id', to: 'posts#checked'
 end
